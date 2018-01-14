b96d32312d388b9b7837f2510629295c07c6cbb1
yason <yheissoneduardo19@gmail.com>
2018-01-14 12:39:43 -0430

    Se le modifico el css para que se deshabilitara el buscador en la pagina de reportes

--- application/views/pages/Index.php
+++ application/views/pages/Index.php
@@ -46,7 +46,7 @@
                 </div>
             </div>
             <!-- #END# Widgets -->
-            <div class="row clearfix">
+            <div class="row clearfix index-tables">
                 <!-- Browser Usage -->
                 <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                     <div class="card">
